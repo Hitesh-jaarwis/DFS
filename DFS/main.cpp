693a5dc3527466a0c7d3356947f0a92986b556a4
Hitesh Arora <hitesh-arora@live.com>
2016-06-14 22:13:30 -0500

    Get complexity of program.

--- DFS/main.cpp
+++ DFS/main.cpp
@@ -1,41 +1,43 @@
 //
 //  main.cpp
-//  DFS
+//  BFS
 //
-//  Created by Hitesh Arora on 5/31/16.
+//  Created by Hitesh Arora on 6/09/16.
 //  Copyright © 2016 Hitesh Arora. All rights reserved.
 //
 
 #include <iostream>
 
 using std::cout;
+using std::cin;
 using std::cerr;
 using std::endl;
-
+using std::random_shuffle;
 
 //----------------------START of BSTNode Class(Binary Search Tree Node)----------------------//
 
 class BSTNode {
     
-    public:
-        BSTNode     (int newValue);
-        void        setLeft  (BSTNode* newLeft);
-        BSTNode*    getLeft  () const;
-        void        setRight (BSTNode* newRight);
-        BSTNode*    getRight () const;
-        void        print    () const;
-        int         getValue () const;
+public:
+    BSTNode     (int newValue);
+    void        setLeft  (BSTNode* newLeft);
+    BSTNode*    getLeft  () const;
+    void        setRight (BSTNode* newRight);
+    BSTNode*    getRight () const;
+    void        print    () const;
+    int         getValue () const;
     
-    private:
-        int         value;
-        BSTNode*    left;
-        BSTNode*    right;
+private:
+    int         value;
+    BSTNode*    left;
+    BSTNode*    right;
 };
 
 //-----Member Functions of BSTNode Class
 
 BSTNode :: BSTNode (int newValue) {
     value = newValue;
+    
     left = 0;
     right = 0;
 }// end constructor
@@ -78,27 +80,27 @@ int BSTNode :: getValue () const {
 //------------------------START of ULLNode Class(Unordered Linked List Node)-----------------//
 
 class ULLNode {
-   
-    public:
-        ULLNode     (BSTNode* newValue);
-        BSTNode*    getValue  () const;
-        int         getIntValue ();
-        ULLNode*    getNext   () const;
-        void        setNext   (ULLNode *newNext);
-        void        clearNode ();
     
-    private:
-        BSTNode*    value;
-        ULLNode*    next;
+public:
+    ULLNode     (BSTNode* newValue);
+    BSTNode*    getValue  () const;
+    //    int         getIntValue ();
+    ULLNode*    getNext   () const;
+    void        setNext   (ULLNode *newNext);
+    void        clearNode ();
+    
+private:
+    BSTNode*    value;
+    ULLNode*    next;
 };
 
 ULLNode :: ULLNode (BSTNode* newValue) {
-
+    
     value = newValue;
 }// end constructor
 
 BSTNode* ULLNode :: getValue () const {
-
+    
     return value;
 }// end getValue
 
@@ -122,18 +124,21 @@ void ULLNode :: clearNode () {
 //--------------------------START of UnorderedLinkedList Class-------------------------------//
 
 class UnorderedLinkedList {
-   
-    public:
-        UnorderedLinkedList ();
-        bool addAtHead      (BSTNode* newHeadValue);
-        bool addAtHead      (int newHeadValue);
-        bool addAtTail      (BSTNode* newTailValue);
-        bool removeFromHead (BSTNode*& oldHeadValue);
-        void print () const;
+    
+public:
+    UnorderedLinkedList ();
+    bool addAtHead      (BSTNode* newHeadValue);
+    bool addAtHead      (int newHeadValue);
+    bool addAtTail      (BSTNode* newTailValue);
+    bool removeFromHead (BSTNode*& oldHeadValue);
+    void print () const;
+    ULLNode* getHead() const;
+    ULLNode* getTail() const;
+    
     //    bool removeFromTail(BSTNode*& oldTailValue); NOT NEEDED NOW
-    private:
-        ULLNode*    head;
-        ULLNode*    tail;
+private:
+    ULLNode*    head;
+    ULLNode*    tail;
 };
 
 UnorderedLinkedList :: UnorderedLinkedList () {
@@ -146,7 +151,7 @@ bool UnorderedLinkedList :: addAtHead (BSTNode* newHeadValue){
     
     bool success     = true;//assume no failure
     ULLNode* newHead = new ULLNode (newHeadValue);
-
+    
     if (newHead != 0) {
         
         if (head == 0){ //Empty List
@@ -168,7 +173,7 @@ bool UnorderedLinkedList :: addAtTail (BSTNode* newTailValue){
     
     bool success     = true;//assume no failure
     ULLNode* newTail = new ULLNode (newTailValue);
-
+    
     if (newTail != 0) {
         
         if (head == 0){ //Empty List
@@ -182,20 +187,21 @@ bool UnorderedLinkedList :: addAtTail (BSTNode* newTailValue){
     }// end newTail != 0
     else// newTail == 0 -> allocation failed
         success = false;
-
+    
     return success;
 }//end addAtTail
 
 bool UnorderedLinkedList :: removeFromHead (BSTNode*& oldHeadValue){
-
+    
     bool success = true;//assume no failure
-
+    
     if (head != 0) {
         
         oldHeadValue = head -> getValue ();
         ULLNode* toBeDeleted = head;
         head = head -> getNext ();
-//        toBeDeleted->setValue(0);
+        //        cout<<"Value parsed:"<<oldHeadValue->getValue()<<endl;
+        //        toBeDeleted->setValue(0);
         toBeDeleted -> setNext (0);
         delete toBeDeleted;
     }// end head != 0
@@ -209,11 +215,11 @@ void UnorderedLinkedList :: print () const {
     
     
     
-//    cout<< "(" << ((left == 0)?0:left -> getValue())  <<")";
-//    cout<<value;
-//    cout<< "(" << ((right == 0)?0:right -> getValue())<<")";
-//    cout<<endl;
-
+    //    cout<< "(" << ((left == 0)?0:left -> getValue())  <<")";
+    //    cout<<value;
+    //    cout<< "(" << ((right == 0)?0:right -> getValue())<<")";
+    //    cout<<endl;
+    
     
     
     if (head != 0) {
@@ -229,25 +235,33 @@ void UnorderedLinkedList :: print () const {
     
 }//end print
 
+ULLNode* UnorderedLinkedList :: getHead() const{
+    return head;
+}//return Head of the list
+
+ULLNode* UnorderedLinkedList :: getTail() const{
+    return tail;
+}//return Tail of the list
+
 //-----------------------------END of UnorderedLinkedList Class------------------------------//
 
 //-----------------------------START of Stack Class------------------------------------------//
 
 class Stack : public UnorderedLinkedList{//is-a relationship
-    public:
+public:
     
-        bool push (BSTNode*  newHeadValue);
-        bool pop  (BSTNode*& oldHeadValue);
+    bool push (BSTNode*  newHeadValue);
+    bool pop  (BSTNode*& oldHeadValue);
 };
 
 bool Stack :: push (BSTNode* newHeadValue){
-
+    
     return addAtHead (newHeadValue);
-//    callAtHead
+    //    callAtHead
 }// end Push to stack
 
 bool Stack :: pop (BSTNode*& oldHeadValue){
-
+    
     return removeFromHead (oldHeadValue);
 }// end Pop from stack
 //----------------------------------END of Stack Class---------------------------------------//
@@ -255,15 +269,15 @@ bool Stack :: pop (BSTNode*& oldHeadValue){
 //-----------------------------START of Queue Class------------------------------------------//
 
 class Queue : public UnorderedLinkedList{
-   
-    public:
     
-        bool add     (BSTNode*  newTailValue);
-        bool remove  (BSTNode*& oldTailValue);
+public:
+    
+    bool add     (BSTNode*  newTailValue);
+    bool remove  (BSTNode*& oldTailValue);
 };
 
 bool Queue :: add (BSTNode* newTailValue){
-
+    
     return addAtTail (newTailValue);
 }// end add to queue
 
@@ -281,7 +295,8 @@ public:
     BinarySearchTree ();
     bool add (int newValue);
     void depthFirstTraversal () const;
-    void breadthFirstTraversal () const;
+    int breadthFirstTraversal (int number) const;
+    BSTNode* getBSTNodeFromQueue (Queue queue) const;
 private:
     BSTNode* root;
 };
@@ -302,11 +317,18 @@ bool BinarySearchTree :: add (int newValue) {
         BSTNode* previous = 0;
         
         while (current != 0) {
-            if (newAdd -> getValue () < current -> getValue ())
-                current = current->getLeft();
-            else
-                current = current->getRight();
+            
+            previous = current;
+            
+            if (newAdd -> getValue () < current -> getValue ()){
+                current = current -> getLeft();
+            }
+            else{
+                current = current -> getRight();
+            }
+            
         }// end while current != 0
+        
         
         if (previous !=0) {
             if (newAdd -> getValue () < previous -> getValue ())
@@ -318,6 +340,7 @@ bool BinarySearchTree :: add (int newValue) {
             root = newAdd;
             current = 0;
         }
+        
         
     }// end newAdd !=0
     else// newAdd == 0 -> allocation failed
@@ -353,31 +376,19 @@ void BinarySearchTree :: depthFirstTraversal () const {
                 else
                     cout<<"Empty stack";
             }
-             //this won't work for the right node of the tree.
+            //this won't work for the right node of the tree.
             current = 0;//Temp
         }
     }
 }// end depthFirstTraversal
 
-/*
-void BinarySearchTree :: breadthFirstTraversal () const {
-    
-    Stack s;
-    Queue q;
-    BSTNode* current = root;
-    while (current != 0) {
-        current ->print();
-        current = current ->getLeft();
-    }
-}
- */
-
-void BinarySearchTree :: breadthFirstTraversal () const {
+int BinarySearchTree :: breadthFirstTraversal (int number) const {
     
     Queue queue;
     BSTNode* current = root;
-    while (current != 0) {
-        //        current ->print();
+    queue.add(root);
+    int count = 0;
+    while (current != NULL) {
         
         //If there is right child, add it to queue
         if (current -> getLeft() != 0)
@@ -387,68 +398,119 @@ void BinarySearchTree :: breadthFirstTraversal () const {
         if (current -> getRight() != 0)
             queue.add(current -> getRight());
         
+        queue.removeFromHead(current);
+        if (current->getValue() == number) {
+            return count;
+        }
+        current = getBSTNodeFromQueue(queue);
+        count++;
     }
+    return count;
 }// end breadthFirstTraversal
 
+
+BSTNode* BinarySearchTree :: getBSTNodeFromQueue (Queue queue) const {
+    BSTNode* bSTNode = NULL;
+    UnorderedLinkedList obj = queue;
+    ULLNode *uLLnode = obj.getHead();
+    if (uLLnode != NULL) {
+        bSTNode =  uLLnode->getValue();
+    }
+    return bSTNode;
+}// end fetching head node from Queue
+
+
+int * storeRandomNumbersInArray(int array[]);
+
+int * storeRandomNumbersInArray(int array[]){
+    for (int i = 1; i<= 1000; i++) {
+        array[i] = i;
+    }
+    random_shuffle(array, array + 1001);
+    
+    /*
+     int tempArr[1000];
+     
+     for(int i=0;i<1000;++i)
+     {
+     int r=rand()%1000;
+     if(!tempArr[r])
+     printf("%d\n",r);
+     else
+     i--;
+     tempArr[r]=1;
+     }
+     for (int i=0; i<1000; i++)
+     cout << tempArr[i] << " ";
+     
+     */
+    return array;
+}
 
 //-----------------------------END of BinarySearchTree Class---------------------------------//
 // START of Main-----
 int main(int argc, const char * argv[]) {
-
+    
     BSTNode n1 (137);
     BSTNode n2 (122);
-//    n1.setLeft (&n2);
+    //    n1.setLeft (&n2);
     BSTNode n3 (116);
-//    n2.setLeft (&n3);
+    //    n2.setLeft (&n3);
     BSTNode n4 (120);
-//    n3.setRight (&n4);
-//    n1.print ();
-//    cout << endl;
-//    n2.print ();
-//    cout << endl;
-//    n3.print ();
-//    cout << endl;
-//    n4.print ();
-//    cout << endl;
+    //    n3.setRight (&n4);
+    //    n1.print ();
+    //    cout << endl;
+    //    n2.print ();
+    //    cout << endl;
+    //    n3.print ();
+    //    cout << endl;
+    //    n4.print ();
+    //    cout << endl;
     
-
-    cout << "Binary Search Tree Traversal\n";
-    BinarySearchTree t1;
-    t1.add (37);
-    t1.depthFirstTraversal ();
-    t1.add (22);
-    t1.depthFirstTraversal ();
-    t1.add (16);
-    t1.depthFirstTraversal ();
-    t1.add (20);
-    t1.depthFirstTraversal ();
+    int arr[1000];
+    arr[1000] = *storeRandomNumbersInArray(arr);
+    
+    //    int numberToSeed = 0;
+    //    cout<<"Enter random number to seed:\n";
+    //    cin>>numberToSeed;
+    
+    BinarySearchTree tree;
+    
+    //add array of random numbers to Binary Search Tree.
+    for (int i = 0; i<1000; i++) {
+        tree.add (arr[i]);
+    }
     
     
+    cout<<"i    BFS    DFS\n";
+    for (int i = 1; i <= 10; i++) {
+        int travCount = tree.breadthFirstTraversal (i*100);
+        cout<<i<<"    "<<travCount<<endl;
+    }
     
+    //    cout<<"Traversal Count:"<<tree.breadthFirstTraversal (759)<<endl;
+    
+    return 0;
     
     
     /*
-    //Working on UnorderedLinkedList & Stack and Queues.
-    BSTNode *ptr = &n1;
-    
-    UnorderedLinkedList l1;
-    l1.addAtHead (&n1);
-    l1.print ();
-    l1.addAtTail(&n2);
-    l1.print ();
-    l1.addAtTail(&n3);
-    l1.print ();
-    l1.addAtHead(&n4);
-    l1.print ();
-    cout<<endl;
-    l1.removeFromHead(ptr);
-    l1.print ();
-
-    */
-    
-    
-
-    
+     //Working on UnorderedLinkedList & Stack and Queues.
+     BSTNode *ptr = &n1;
+     
+     UnorderedLinkedList l1;
+     l1.addAtHead (&n1);
+     l1.print ();
+     l1.addAtTail(&n2);
+     l1.print ();
+     l1.addAtTail(&n3);
+     l1.print ();
+     l1.addAtHead(&n4);
+     l1.print ();
+     cout<<endl;
+     l1.removeFromHead(ptr);
+     l1.print ();
+     
+     */
     
     return 0;
 }//end main
